7332cb02447330ae4b3bbaa14c6f6ee1c5acfe68
Eaint-Thayaphy-Oo <eaintthayaphyoo098@gmail.com>
2024-01-11 15:38:09 +0630

    database commit

---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //dashboard
+    public function index()
+    {
+        return "nice";
+    }
+}
